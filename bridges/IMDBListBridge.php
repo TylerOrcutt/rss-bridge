c7dd53322eb601f98e16c3d93ff3d485ab407113
TylerOrcutt <tyoructt@gmail.com>
2020-12-07 05:39:15 +0000

    IMDB movie list feed

---
+++ bridges/IMDBListBridge.php
@@ -0,0 +1,56 @@
+<?php
+class IMDBListBridge extends BridgeAbstract {
+		
+	const MAINTAINER="yourmom";
+	const NAME = "IMDB Lists";
+	const URI = "https://www.imdb.com";
+	const DESCRIPTION = "returns w/e list from imdb";
+	const CACHE_TIMEOUT = 2;
+//	const PARAMETERS = array( array(
+//		'ID' => array(
+//			'name' =>'ID',
+//			'exampleValue'=>'ls12345',
+//		),
+//	));
+
+	public function collectData() {
+
+		//$listID = $this->getInput('ID');
+		$url = self::URI .
+			'/list/' .
+			'ls016522954'.
+			'/' .
+			'?st_dt=&mode=detail&page=1&ref_=ttls_vm_dtl&title_type=movie&sort=list_order,asc';
+
+		$item = array();
+		$item['title'] = 'test';
+		$item['uri'] = $url;
+		$this->items[] = $item;
+		
+	//	echo '$url';
+		$html = getSimpleHTMLDOM(
+			$url) or returnServerError('Could request IMDB');
+
+		if($html->find('div.lister-list',0)==false){
+			returnServerError('List not found');
+		}
+
+		foreach($html->find('div.lister-item') as $ele){
+			$item = array();
+
+			$item['title'] = $ele->find('a',1)->plaintext . $ele->find('span',1)->plaintext;
+			$item['uri'] = str_replace('?ref_=ttls_li_i','',self::URI.$ele->find('a',0)->href);
+			$item['content'] = str_replace('https://www.imdb.com     ','',self::URI.$ele->find('p',1)->plaintext);
+			$item['rating'] = $ele->find('p',0)->find('span',0)->innertext;
+			$item['runtime'] = $ele->find('p',0)->find('span',2)->innertext;
+
+			if(isset($item['title'])){
+				$this->items[] = $item;
+			}
+			
+		}
+	}
+
+}
+
+
